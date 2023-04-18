ec7125fe887fd29b8433e2463dde7f1bc28a3b7a
terrenceperez <terrence.perez@obf.ateneo.edu>
2023-04-18 18:52:15 +0800

    Structs

---
+++ contracts/StakeholderSpending.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract StakeholderSpending {
+
+    struct OSA {
+        string name;
+        uint contact;
+        uint balance;
+        address struct_address;
+    }
+
+    struct StudentOrganization {
+        string name;
+        uint contact;
+        uint balance;
+        address struct_address;
+    }
+
+    struct Supplier {
+        string name;
+        uint contact;
+        uint balance;
+        address struct_address;
+    }
+
+
+}
