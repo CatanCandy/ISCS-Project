23961b51a0afa78c283e6cd88b51e2d38cfe269b
terrenceperez <terrence.perez@obf.ateneo.edu>
2023-04-27 11:43:07 +0800

    Added transaction modifiers

--- contracts/OSAToken.sol
+++ contracts/OSAToken.sol
@@ -22,32 +22,53 @@ contract OSAToken is ERC20, AccessControl {
     }
 
     // Access Modifiers
-    modifier isOSA{
+    modifier isOSA { // restrict function to OSA entities
         require(hasRole(OSA_ROLE, msg.sender), "You are not an authorized OSA entity.");
         _;
     }
-    modifier isORG{
+    modifier isORG { // restrict function to organization entities
         require(hasRole(ORG_ROLE, msg.sender), "You are not an authorized organization entity.");
         _;
     }
-    modifier isSUP{
+    modifier isSUP { // restrict function to supplier entities
         require(hasRole(SUP_ROLE, msg.sender), "You are not an authorized supplier entity.");
+        _;
+    }
+    modifier onlyOSAtoORG(address sender, address recipient) { // restrict transactions from OSA entities to organization entities
+        require(hasRole(OSA_ROLE, sender), "You are not an authorized OSA entity.");
+        require(hasRole(ORG_ROLE, recipient), "The recipient is not an authorized organization entity.");
+        _;
+    }
+    modifier onlyORGtoSUP(address sender, address recipient) { // restrict transactions from OSA entities to organization entities
+        require(hasRole(ORG_ROLE, sender), "You are not an authorized organization entity.");
+        require(hasRole(SUP_ROLE, recipient), "The recipient is not an authorized supplier entity.");
         _;
     }
 
     // Role Granting Functions
+    
+    // sanity check: if address has existing role
+    function _revokeExistingRole(address account) internal isOSA {
+        revokeRole(DEFAULT_ADMIN_ROLE, account);
+        revokeRole(OSA_ROLE, account);
+        revokeRole(ORG_ROLE, account);
+        revokeRole(SUP_ROLE, account);
+    }
 
     function _addOSA(string memory name, address account) external isOSA {
         _addressToName[account] = name;
+        _revokeExistingRole(account);
         grantRole(DEFAULT_ADMIN_ROLE, account);
         grantRole(OSA_ROLE, account);
     }
     function _addOrganization(string memory name, address account) external isOSA {
         _addressToName[account] = name;
+        _revokeExistingRole(account);
         grantRole(ORG_ROLE, account);
     }
     function _addSupplier(string memory name, address account) external isOSA {
         _addressToName[account] = name;
+        _revokeExistingRole(account);
         grantRole(SUP_ROLE, account);
     }
 
@@ -60,29 +81,29 @@ contract OSAToken is ERC20, AccessControl {
 
     // Transaction Functions
 
-    function _increaseOSABudget(uint256 amount) external isOSA {
+    function _increaseOSABudget(uint256 amount) external isOSA { // increase current OSA budget
         _mint(msg.sender, amount);
         emit IncreaseOSABudget(_addressToName[msg.sender], amount);
     }
 
-    function _spend(address supplier, uint256 amount) external isORG { // transfer tokens from ORG to SUP
+    function _spend(address supplier, uint256 amount) external onlyORGtoSUP(msg.sender, supplier) { // transfer tokens from ORG to SUP
         _transfer(msg.sender, supplier, amount);
         emit Spend(_addressToName[msg.sender], _addressToName[supplier], amount);
     }
 
-    function _allocate(address organization, uint256 amount) external isOSA { // transfer tokens from OSA to ORG
+    function _allocate(address organization, uint256 amount) external onlyOSAtoORG(msg.sender, organization) { // transfer tokens from OSA to ORG
         _transfer(msg.sender, organization, amount);
         emit Allocate(_addressToName[msg.sender], _addressToName[organization], amount);
     }
 
-    function _deduct(address organization, uint256 amount) external isOSA { // transfer tokens from ORG to OSA
+    function _deduct(address organization, uint256 amount) external onlyOSAtoORG(msg.sender, organization) { // transfer ORG tokens back to OSA
         _transfer(organization, msg.sender, amount);
         emit Deduct(_addressToName[organization], _addressToName[msg.sender], amount);
     }
 
     function _cashout() external isSUP { // cashout all of SUP tokens
-        _burn(msg.sender, balanceOf(msg.sender));
         emit Cashout(_addressToName[msg.sender], balanceOf(msg.sender));
+        _burn(msg.sender, balanceOf(msg.sender));
     }
 
     function _myBalance() external view returns(uint256) { // view current account's token balance
